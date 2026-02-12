6d4e6d1a6c3c57af9095ced74d140ca5fbf0d3cd
seunggabi <seunggabi@naver.com>
2026-02-13 08:11:11 +0900

    Add MacScreen cask - window management app

---
+++ Casks/mac-screen.rb
@@ -0,0 +1,47 @@
+cask "mac-screen" do
+  version "0.1.0"
+  sha256 :no_check  # Will be updated after first DMG release
+
+  url "https://github.com/seunggabi/mac-screen/releases/download/v#{version}/MacScreen-#{version}.dmg"
+  name "MacScreen"
+  desc "macOS window management app with flexible screen splitting"
+  homepage "https://github.com/seunggabi/mac-screen"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "MacScreen.app"
+
+  postflight do
+    system_command "#{appdir}/MacScreen.app/Contents/MacOS/MacScreen",
+                   args: ["--version"],
+                   print_stderr: false
+  end
+
+  uninstall quit: "com.seunggabi.mac-screen"
+
+  zap trash: [
+    "~/Library/Preferences/com.seunggabi.mac-screen.plist",
+    "~/Library/Caches/com.seunggabi.mac-screen",
+    "~/Library/Application Support/MacScreen",
+    "~/Library/Logs/MacScreen",
+  ]
+
+  caveats <<~EOS
+    MacScreen requires Accessibility permission to manage windows.
+
+    On first launch, you will be prompted to grant permission in:
+    System Settings > Privacy & Security > Accessibility
+
+    Default keyboard shortcuts:
+    - Ctrl+Option+Left/Right: Left/Right half
+    - Ctrl+Option+Return: Maximize
+    - Ctrl+Option+C: Center
+    - Ctrl+Option+Q/W/A/S: Quarters
+
+    For more information, visit:
+    https://github.com/seunggabi/mac-screen
+  EOS
+end
